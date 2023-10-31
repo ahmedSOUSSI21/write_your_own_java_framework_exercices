b2056d7e78e42241e6a12b7ef2e5a8d7a8dd1aa2
Ahmed SOUSSI <soussiahmed62@gmail.com>
2023-10-31 09:04:37 +0100

    ORM finished

--- orm/src/main/java/com/github/forax/framework/orm/ORM.java
+++ orm/src/main/java/com/github/forax/framework/orm/ORM.java
@@ -184,7 +184,6 @@ public final class ORM {
                       .stream()
                       .findFirst();
             }
-
             throw new IllegalStateException("unknown method " + method);
           }
         };
